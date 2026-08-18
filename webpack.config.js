57db8c923a0460c4034aa557b8e3ed257960d0aa
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-08-18 08:16:50 -0300

    correção build

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, "src", "assets"),
-          to: path.resolve(__dirname, "dist", "src", "assets"),
+          to: path.resolve(__dirname, "dist", "assets"),
         },
       ],
     }),
